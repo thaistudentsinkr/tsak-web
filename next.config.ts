6dd0e1f1b556e979a6b2902adc6229cb91ea5a21
Passavee Losripat <passavee.losripat@gmail.com>
2025-10-10 23:51:47 +0900

    feat(ui): add static contact us page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["upload.wikimedia.org", "encrypted-tbn0.gstatic.com", "images.icon-icons.com"],
+  },
 };
 
 export default nextConfig;
